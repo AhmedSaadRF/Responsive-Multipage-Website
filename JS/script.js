27232dc28f2c54df2e45428ef1cfceb9f1eaf650
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-06-05 03:30:41 +0300

    Upload Project

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll',()=>{
@@ -8,14 +7,3 @@ window.addEventListener('scroll',()=>{
     navbar.classList.remove('sticky')
   }
 })
-=======
-const navbar = document.querySelector('.navbar');
-
-window.addEventListener('scroll',()=>{
-  if(window.scrollY>0) {
-    navbar.classList.add('sticky')
-  } else {
-    navbar.classList.remove('sticky')
-  }
-})
->>>>>>> 61eee6341aef6503e185faba2dd1f6db6ad6b6ea
